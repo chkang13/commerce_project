0ede40c2ad66d63282b13aecfaec8f87b6da59eb
chkang13 <chkang31@naver.com>
2024-05-04 05:53:29 +0900

    Add CircuitBreaker 적용

--- member-service/src/main/java/com/my/memberservice/client/OrderServiceFeignClient.java
+++ member-service/src/main/java/com/my/memberservice/client/OrderServiceFeignClient.java
@@ -1,11 +1,13 @@
 package com.my.memberservice.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "order-service", url = "http://localhost:8082")
+@CircuitBreaker(name = "addWish")
 public interface OrderServiceFeignClient {
     @RequestMapping(method = RequestMethod.POST, value = "/wishes/{memberId}", consumes = "application/json")
     String addWish(@PathVariable("memberId") Long memberId);
--- order-service/src/main/java/com/my/orderservice/client/ProductServiceFeignClient.java
+++ order-service/src/main/java/com/my/orderservice/client/ProductServiceFeignClient.java
@@ -1,12 +1,14 @@
 package com.my.orderservice.client;
 
 import com.my.orderservice.dto.client.GetProductResDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "product-service", url = "http://localhost:8081")
+@CircuitBreaker(name = "getProduct")
 public interface ProductServiceFeignClient {
     @RequestMapping(method = RequestMethod.GET, value = "/products/internal/{productId}", consumes = "application/json")
     GetProductResDTO getInProduct(@PathVariable("productId") Long productId);
