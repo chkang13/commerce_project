598443c85b1bcc15681ed5469d9778ba0519ca9d
chkang13 <chkang31@naver.com>
2024-06-19 16:53:32 +0900

    Modify: 예외 처리 메세지 보이도록 변경

--- core-service/src/main/java/com/my/coreservice/global/common/BaseException.java
+++ core-service/src/main/java/com/my/coreservice/global/common/BaseException.java
@@ -7,8 +7,12 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class BaseException extends RuntimeException {
     private BaseResponseStatus status;
+
+    public BaseException(BaseResponseStatus status) {
+        super(status.getMessage());
+        this.status = status;
+    }
 }
